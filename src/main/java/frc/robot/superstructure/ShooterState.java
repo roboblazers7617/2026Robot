86519ec6b896d39e2a52c7813261ab9b9d528742
Apollo Nargang <CoffeeCoder1@outlook.com>
2026-01-28 22:59:52 -0500

    Add a basic shell of the superstructure

---
+++ src/main/java/frc/robot/superstructure/ShooterState.java
@@ -0,0 +1,110 @@
+package frc.robot.superstructure;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Radians;
+
+/**
+ * An object that represents a state that the shooter can be in. This includes the shooter speed, turret position, and hood position.
+ */
+public class ShooterState {
+	private AngularVelocity shooterSpeed;
+	private Angle turretAngle;
+	private Angle hoodAngle;
+
+	/**
+	 * Creates a new ShooterState with zero values.
+	 */
+	public ShooterState() {
+		shooterSpeed = RadiansPerSecond.of(0);
+		turretAngle = Radians.of(0);
+		hoodAngle = Radians.of(0);
+	}
+
+	/**
+	 * Creates a new ShooterState with specified values.
+	 *
+	 * @param shooterSpeed
+	 *            The velocity to spin the shooter up to.
+	 * @param turretAngle
+	 *            The angle to point the turret at.
+	 * @param hoodAngle
+	 *            The angle to set the hood to.
+	 */
+	public ShooterState(AngularVelocity shooterSpeed, Angle turretAngle, Angle hoodAngle) {
+		this.shooterSpeed = shooterSpeed;
+		this.turretAngle = turretAngle;
+		this.hoodAngle = hoodAngle;
+	}
+
+	/**
+	 * Sets the shooter speed.
+	 *
+	 * @param shooterSpeed
+	 *            The velocity to spin the shooter up to.
+	 * @return
+	 *         This object for method chaining.
+	 */
+	public ShooterState setShooterSpeed(AngularVelocity shooterSpeed) {
+		this.shooterSpeed = shooterSpeed;
+		return this;
+	}
+
+	/**
+	 * Sets the turret angle.
+	 *
+	 * @param turretAngle
+	 *            The angle to point the turret at.
+	 * @return
+	 *         This object for method chaining.
+	 */
+	public ShooterState setTurretAngle(Angle turretAngle) {
+		this.turretAngle = turretAngle;
+		return this;
+	}
+
+	/**
+	 * Sets the hood angle.
+	 *
+	 * @param hoodAngle
+	 *            The angle to set the hood to.
+	 * @return
+	 *         This object for method chaining.
+	 */
+	public ShooterState setHoodAngle(Angle hoodAngle) {
+		this.hoodAngle = hoodAngle;
+		return this;
+	}
+
+	/**
+	 * Gets the shooter speed.
+	 *
+	 * @return
+	 *         The shooter speed.
+	 */
+	public AngularVelocity getShooterSpeed() {
+		return shooterSpeed;
+	}
+
+	/**
+	 * Gets the turret angle.
+	 *
+	 * @return
+	 *         The turret angle.
+	 */
+	public Angle getTurretAngle() {
+		return turretAngle;
+	}
+
+	/**
+	 * Gets the hood angle.
+	 *
+	 * @return
+	 *         The hood angle.
+	 */
+	public Angle getHoodAngle() {
+		return hoodAngle;
+	}
+}
