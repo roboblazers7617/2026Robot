1a502b897c3472eb165ea69dd9576efd0e2aa1c3
A-Person323 <Caleb@thereeds.net>
2026-01-30 17:09:19 -0500

    Put back team specific basics into Test Chassis Repo
    changed robot and robot container to align with main base

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,80 +4,163 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.HootAutoReplay;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.logging.EpilogueBackend;
+import edu.wpi.first.epilogue.logging.NTEpilogueBackend;
+import edu.wpi.first.net.WebServer;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
+import com.ctre.phoenix6.HootEpilogueBackend;
+import com.ctre.phoenix6.SignalLogger;
+
+import edu.wpi.first.epilogue.Epilogue;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.LoggingConstants;
 
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+@Logged
 public class Robot extends TimedRobot {
-    private Command m_autonomousCommand;
+	/**
+	 * Command that contains the autonomous routine. Set and run at the start of {@link #autonomousInit()}.
+	 */
+	private Command autonomousCommand;
+	/**
+	 * Class that contains most of the robot initialization and control logic.
+	 */
+	private final RobotContainer robotContainer;
 
-    private final RobotContainer m_robotContainer;
+	@Logged
+	private final PowerDistribution powerDistribution = new PowerDistribution();
 
-    /* log and replay timestamp and joystick data */
-    private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
-        .withTimestampReplay()
-        .withJoystickReplay();
+	/**
+	 * This function is run when the robot is first started up and should be used for any
+	 * initialization code.
+	 */
+	public Robot() {
+		DataLogManager.start();
+		DriverStation.startDataLog(DataLogManager.getLog());
+		Epilogue.configure(config -> {
+			if (LoggingConstants.DEBUG_MODE) {
+				// If in debug mode, write data to NetworkTables as well as SignalLogger
+				config.backend = EpilogueBackend.multi(new HootEpilogueBackend(), new NTEpilogueBackend(NetworkTableInstance.getDefault()));
+			} else {
+				// Otherwise just write to SignalLogger
+				config.backend = new HootEpilogueBackend();
+			}
 
-    public Robot() {
-        m_robotContainer = new RobotContainer();
-    }
+			config.root = "Telemetry";
+			config.minimumImportance = LoggingConstants.DEBUG_LEVEL;
+		});
+		Epilogue.bind(this);
 
-    @Override
-    public void robotPeriodic() {
-        m_timeAndJoystickReplay.update();
-        CommandScheduler.getInstance().run(); 
-    }
+		// Start CTRE SignalLogger
+		SignalLogger.start();
 
-    @Override
-    public void disabledInit() {}
+		// Start a WebServer that hosts the deploy directory
+		// This is used for the Elastic layout
+		WebServer.start(5800, Filesystem.getDeployDirectory().getPath());
 
-    @Override
-    public void disabledPeriodic() {}
+		// Instantiate our RobotContainer. This will perform all our button bindings, and put our
+		// autonomous chooser on the dashboard.
+		robotContainer = new RobotContainer();
+	}
 
-    @Override
-    public void disabledExit() {}
+	/**
+	 * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+	 * that you want ran during disabled, autonomous, teleoperated and test.
+	 * <p>
+	 * This runs after the mode specific periodic functions, but before LiveWindow and
+	 * SmartDashboard integrated updating.
+	 */
+	@Override
+	public void robotPeriodic() {
+		// Runs the Scheduler. This is responsible for polling buttons, adding newly-scheduled
+		// commands, running already-scheduled commands, removing finished or interrupted commands,
+		// and running subsystem periodic() methods. This must be called from the robot's periodic
+		// block in order for anything in the Command-based framework to work.
+		CommandScheduler.getInstance().run();
+	}
 
-    @Override
-    public void autonomousInit() {
-        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 */
+	@Override
+	public void disabledInit() {}
 
-        if (m_autonomousCommand != null) {
-            CommandScheduler.getInstance().schedule(m_autonomousCommand);
-        }
-    }
+	@Override
+	public void disabledPeriodic() {}
 
-    @Override
-    public void autonomousPeriodic() {}
+	/**
+	 * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+	 */
+	@Override
+	public void autonomousInit() {
+		robotContainer.autoInit();
 
-    @Override
-    public void autonomousExit() {}
+		autonomousCommand = robotContainer.getAutonomousCommand();
 
-    @Override
-    public void teleopInit() {
-        if (m_autonomousCommand != null) {
-            CommandScheduler.getInstance().cancel(m_autonomousCommand);
-        }
-    }
+		// Run the autonomous command
+		if (autonomousCommand != null) {
+			CommandScheduler.getInstance().schedule(autonomousCommand);
+		}
+	}
 
-    @Override
-    public void teleopPeriodic() {}
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {}
 
-    @Override
-    public void teleopExit() {}
+	@Override
+	public void teleopInit() {
+		robotContainer.teleopInit();
 
-    @Override
-    public void testInit() {
-        CommandScheduler.getInstance().cancelAll();
-    }
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null) {
+			autonomousCommand.cancel();
+		}
+	}
 
-    @Override
-    public void testPeriodic() {}
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {}
 
-    @Override
-    public void testExit() {}
+	@Override
+	public void testInit() {
+		// Cancels all running commands at the start of test mode.
+		CommandScheduler.getInstance().cancelAll();
+	}
 
-    @Override
-    public void simulationPeriodic() {}
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {}
+
+	/**
+	 * This function is called once when the robot is first started up.
+	 */
+	@Override
+	public void simulationInit() {}
+
+	/**
+	 * This function is called periodically whilst in simulation.
+	 */
+	@Override
+	public void simulationPeriodic() {}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,95 +4,159 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.*;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.Constants.DashboardConstants;
+import frc.robot.Constants.LoggingConstants;
+import frc.robot.util.Elastic;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
-import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
-
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+@Logged
 public class RobotContainer {
-    private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+	private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+	private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
-    /* Setting up bindings for necessary control of the swerve drive platform */
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+	/* Setting up bindings for necessary control of the swerve drive platform */
+	private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+			.withDeadband(MaxSpeed * 0.1)
+			.withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+			.withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+	private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+	private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-    private final Telemetry logger = new Telemetry(MaxSpeed);
+	private final Telemetry logger = new Telemetry(MaxSpeed);
+	public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+	/**
+	 * The Controller used by the Driver of the robot, primarily controlling the drivetrain.
+	 */
+	@NotLogged
+	private final CommandXboxController driverController = new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
+	/**
+	 * The Controller used by the Operator of the robot, primarily controlling the superstructure.
+	 */
+	@NotLogged
+	private final CommandXboxController operatorController = new CommandXboxController(OperatorConstants.OPERATOR_CONTROLLER_PORT);
 
-    private final CommandXboxController joystick = new CommandXboxController(0);
+	/**
+	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 */
+	public RobotContainer() {
+		// Publish version metadata
+		VersionConstants.publishNetworkTables(NetworkTableInstance.getDefault().getTable("/Metadata"));
+		VersionConstants.logSignals();
 
-    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+		// Configure the trigger bindings
+		configureNamedCommands();
+		configureDriverControls();
+		configureOperatorControls();
+	}
 
-    public RobotContainer() {
-        configureBindings();
-    }
+	/**
+	 * This method is run at the start of Auto.
+	 */
+	public void autoInit() {
+		// Set the Elastic tab
+		if (!LoggingConstants.DEBUG_MODE) {
+			Elastic.selectTab(DashboardConstants.AUTO_TAB_NAME);
+		}
+	}
 
-    private void configureBindings() {
-        // Note that X is defined as forward according to WPILib convention,
-        // and Y is defined as to the left according to WPILib convention.
-        drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-            )
-        );
+	/**
+	 * This method is run at the start of Teleop.
+	 */
+	public void teleopInit() {
+		// Set the Elastic tab
+		if (!LoggingConstants.DEBUG_MODE) {
+			Elastic.selectTab(DashboardConstants.TELEOP_TAB_NAME);
+		}
+	}
 
-        // Idle while the robot is disabled. This ensures the configured
-        // neutral mode is applied to the drive motors while disabled.
-        final var idle = new SwerveRequest.Idle();
-        RobotModeTriggers.disabled().whileTrue(
-            drivetrain.applyRequest(() -> idle).ignoringDisable(true)
-        );
+	/**
+	 * Sets up the {@link NamedCommands} used by the autonomous routine.
+	 */
+	private void configureNamedCommands() {}
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+	/**
+	 * Configures {@link Trigger Triggers} to bind Commands to the Driver Controller buttons.
+	 */
+	private void configureDriverControls() {
+		// Note that X is defined as forward according to WPILib convention,
+		// and Y is defined as to the left according to WPILib convention.
+		drivetrain.setDefaultCommand(
+				// Drivetrain will execute this command periodically
+				drivetrain.applyRequest(() -> drive.withVelocityX(-driverController.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+						.withVelocityY(-driverController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+						.withRotationalRate(-driverController.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+				));
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+		// Idle while the robot is disabled. This ensures the configured
+		// neutral mode is applied to the drive motors while disabled.
+		final var idle = new SwerveRequest.Idle();
+		RobotModeTriggers.disabled().whileTrue(drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
-        // Reset the field-centric heading on left bumper press.
-        joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+		driverController.a().whileTrue(drivetrain.applyRequest(() -> brake));
+		driverController.b().whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))));
 
-        drivetrain.registerTelemetry(logger::telemeterize);
-    }
+		// Run SysId routines when holding back/start and X/Y.
+		// Note that each routine should be run exactly once in a single log.
+		driverController.back().and(driverController.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+		driverController.back().and(driverController.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+		driverController.start().and(driverController.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+		driverController.start().and(driverController.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
-    public Command getAutonomousCommand() {
-        // Simple drive forward auton
-        final var idle = new SwerveRequest.Idle();
-        return Commands.sequence(
-            // Reset our field centric heading to match the robot
-            // facing away from our alliance station wall (0 deg).
-            drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
-            // Then slowly drive forward (away from us) for 5 seconds.
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(0.5)
-                    .withVelocityY(0)
-                    .withRotationalRate(0)
-            )
-            .withTimeout(5.0),
-            // Finally idle for the rest of auton
-            drivetrain.applyRequest(() -> idle)
-        );
-    }
+		// Reset the field-centric heading on left bumper press.
+		driverController.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+
+		drivetrain.registerTelemetry(logger::telemeterize);
+	}
+
+	/**
+	 * Configures {@link Triggers} to bind Commands to the Operator Controller buttons.
+	 */
+	private void configureOperatorControls() {}
+
+	/**
+	 * Use this to pass the autonomous command to the main {@link Robot} class.
+	 *
+	 * @return the command to run in autonomous
+	 */
+	public Command getAutonomousCommand() {
+		// Simple drive forward auton
+		final var idle = new SwerveRequest.Idle();
+		return Commands.sequence(
+				// Reset our field centric heading to match the robot
+				// facing away from our alliance station wall (0 deg).
+				drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
+				// Then slowly drive forward (away from us) for 5 seconds.
+				drivetrain.applyRequest(() -> drive.withVelocityX(0.5)
+						.withVelocityY(0)
+						.withRotationalRate(0))
+						.withTimeout(5.0),
+				// Finally idle for the rest of auton
+				drivetrain.applyRequest(() -> idle));
+	}
 }
