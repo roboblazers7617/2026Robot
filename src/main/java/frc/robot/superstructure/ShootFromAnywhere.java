31e3cf14459b8c263d8c64a2df62c6e8fe708974
Apollo Nargang <CoffeeCoder1@outlook.com>
2026-02-01 16:15:46 -0500

    Superstructure refactoring
    Shuffle some things around to prepare for actual superstructure logic.

--- src/main/java/frc/robot/superstructure/ShootFromAnywhere.java
+++ src/main/java/frc/robot/superstructure/ShootFromAnywhere.java
@@ -1,9 +1,12 @@
 package frc.robot.superstructure;
 
+import java.util.Optional;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.Angle;
+import frc.robot.Constants.FieldConstants;
 
 public class ShootFromAnywhere {
 	/**
@@ -45,5 +48,33 @@ public class ShootFromAnywhere {
 				.getAngle()
 				.rotateBy(Rotation2d.k180deg)
 				.getMeasure();
+	}
+
+	/**
+	 * Gets the desired target pose for the given robot position.
+	 *
+	 * @return
+	 *         Hub receptacle if viable, on to our side if in the center, or empty if otherwise.
+	 */
+	public static Optional<Pose3d> getTargetPoseForPosition(Pose2d robotPose) {
+		// Hubs
+		if (FieldConstants.ShootingZones.HUB_ZONE_RED.contains(robotPose.getTranslation())) {
+			return Optional.of(FieldConstants.ShootingZones.HUB_POSE.getRedPose());
+		}
+
+		if (FieldConstants.ShootingZones.HUB_ZONE_BLUE.contains(robotPose.getTranslation())) {
+			return Optional.of(FieldConstants.ShootingZones.HUB_POSE.getBluePose());
+		}
+
+		// Neutral zone
+		if (FieldConstants.ShootingZones.NEUTRAL_ZONE_TOP.contains(robotPose.getTranslation())) {
+			return Optional.of(FieldConstants.ShootingZones.SHUTTLE_TOP_POSE.getPoseByAlliance());
+		}
+
+		if (FieldConstants.ShootingZones.NEUTRAL_ZONE_BOTTOM.contains(robotPose.getTranslation())) {
+			return Optional.of(FieldConstants.ShootingZones.SHUTTLE_BOTTOM_POSE.getPoseByAlliance());
+		}
+
+		return Optional.empty();
 	}
 }
