412a0cb6c8deb5ba2647bea2ae98c83a7e60b876
Icarus253 <joshuanorthwest5@gmail.com>
2026-02-21 15:28:26 -0500

    Renamed some Cam names again

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -91,7 +91,7 @@ public final class Constants {
 		public static final Transform3d ROBOT_TO_FRONT_CAM_TRANSFORM = new Transform3d(new Translation3d(0.5, 0.0, 0.0), new Rotation3d(0, 0, 0));
 		public static final String SIDE_CAM_NAME = "CamSide";
 		public static final Transform3d ROBOT_TO_SIDE_CAM_TRANSFORM = new Transform3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d(0, 0, 0));
-		public static final String EXTRA_CAM_NAME = "Cam:3"; //replace the String in the Constant of the camera you want to swap out with the extra cam name
+		public static final String XTRA_CAM_NAME = "CamX3"; // replace the String in the Constant of the camera you want to swap out with the extra cam name
 		public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(4, 4, 8);
 		public static final Matrix<N3, N1> MULTI_TAG_STD_DEVS = VecBuilder.fill(0.5, 0.5, 1);
 	}
