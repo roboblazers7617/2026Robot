d4174fb1691562f3e2d40b26b931e8d3c3e1504b
A-Person323 <Caleb@thereeds.net>
2026-03-15 12:44:36 -0400

    removed import

--- src/main/java/frc/robot/subsystems/HopperUptake.java
+++ src/main/java/frc/robot/subsystems/HopperUptake.java
@@ -14,7 +14,6 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
