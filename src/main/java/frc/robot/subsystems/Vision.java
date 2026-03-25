2850533a32a254671393856b469acdb4b4d556dc
fodfodfod2 <crileylawyer@gmail.com>
2026-03-25 14:51:33 -0400

    Add a toggle for Vision print statements

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -56,9 +56,11 @@ public class Vision extends SubsystemBase {
 				for (int tgt = 0; tgt < result.getTargets().size(); tgt++) {
 					if (result.getTargets().get((tgt)).getPoseAmbiguity() <= 0.2) {
 						drivetrain.addVisionMeasurement(est.estimatedPose.toPose2d(), est.timestampSeconds, estStdDevs);
-						System.out.println("turret measurement");
+						if (VisionConstants.DEBUG_PRINT_STATEMENTS)
+							System.out.println("turret measurement");
 					} else {
-						System.out.println("turret rejected");
+						if (VisionConstants.DEBUG_PRINT_STATEMENTS)
+							System.out.println("turret rejected");
 					}
 				}
 			}
@@ -79,9 +81,11 @@ public class Vision extends SubsystemBase {
 				for (int tgt = 0; tgt < result.getTargets().size(); tgt++) {
 					if (result.getTargets().get((tgt)).getPoseAmbiguity() <= 0.2) {
 						drivetrain.addVisionMeasurement(est.estimatedPose.toPose2d(), est.timestampSeconds, estStdDevs);
-						System.out.println("eboard measurement");
+						if (VisionConstants.DEBUG_PRINT_STATEMENTS)
+							System.out.println("eboard measurement");
 					} else {
-						System.out.println("eboard rejected");
+						if (VisionConstants.DEBUG_PRINT_STATEMENTS)
+							System.out.println("eboard rejected");
 					}
 				}
 			}
